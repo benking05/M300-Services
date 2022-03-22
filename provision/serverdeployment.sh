2e2620a6ba6734124543fa6b077aa16f0767bca3
benking05 <100407826+benking05@users.noreply.github.com>
2022-03-22 08:55:47 +0100

    Update serverdeployment.sh

--- provision/serverdeployment.sh
+++ provision/serverdeployment.sh
@@ -5,3 +5,8 @@ sudo apt-get update
 
 echo "---INSTALLING SAMBA"
 sudo apt-get install samba -y
+sudo systemctl stop nmbd.service
+sudo systemctl disable nmbd.service
+
+echo "---STOPPING SMBD---"
+sudo systemctl stop smbd.service
